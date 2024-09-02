9c7eafab3546ecd3ed57ecde9c8cfff7f6c5139e
vntero <shin.antero@gmail.com>
2024-09-02 16:01:39 +0200

    adds struct for register

--- api/server.go
+++ api/server.go
@@ -14,3 +14,24 @@ type BookShowInput struct {
 	Budget			int64 `json:"budget"`
 	Comment			string `json:"comment"`
 }
+
+type RegisterInput struct {
+	Name 					string `json:"name"`
+	Birthday 				time.Time `json:"birthday"`
+	Origin					string `json:"origin"`
+	Motivation				string `json:"motivation"`
+	ParentOneName			string `json:"parent_one_name"`
+	ParentOneEmail			string `json:"parent_one_email"`
+	ParentOnePhone			int64 `json:"parent_one_phone"`
+	ParentOneStreet			string `json:"parent_one_street"`
+	ParentOneHouseNumber	int64 `json:"parent_one_house_number"`
+	ParentOnePostcode		string `json:"parent_one_postcode"`
+	ParentOneLocation		string `json:"parent_one_location"`
+	ParentTwoName			string `json:"parent_two_name"`
+	ParentTwoEmail			string `json:"parent_two_email"`
+	ParentTwoPhone			int64 `json:"parent_two_phone"`
+	ParentTwoStreet			string `json:"parent_two_street"`
+	ParentTwoHouseNumber	int64 `json:"parent_two_house_number"`
+	ParentTwoPostcode		string `json:"parent_two_postcode"`
+	ParentTwoLocation		string `json:"parent_two_location"`
+}
