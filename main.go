e68791897876b245d8e3e66e08728c88a02c48b1
vntero <shin.antero@gmail.com>
2024-09-05 10:41:35 +0200

    splits code

--- main.go
+++ main.go
@@ -1,45 +1,11 @@
 package main
 
 import (
+	"cafeaulait-server/models"
+	"fmt"
 	"log"
 	"net/http"
 )
-
-
-type BookShowInput struct {
-	Name           string    `json:"name"`
-	Phone          int64     `json:"phone"`
-	Email          string    `json:"email"`
-	Location       string    `json:"location"`
-	Duration       int64     `json:"duration"`
-	NumberOfGuests int64     `json:"number_of_guests"`
-	EventDate      string `json:"event_date"`
-	EventTime      string `json:"event_time"`
-	Budget         int64     `json:"budget"`
-	Comment        string    `json:"comment"`
-}
-
-type RegisterInput struct {
-	Name                 string    `json:"name"`
-	Birthday             string `json:"birthday"`
-	Origin               string    `json:"origin"`
-	Motivation           string    `json:"motivation"`
-	ParentOneName        string    `json:"parent_one_name"`
-	ParentOneEmail       string    `json:"parent_one_email"`
-	ParentOnePhone       int64     `json:"parent_one_phone"`
-	ParentOneStreet      string    `json:"parent_one_street"`
-	ParentOneHouseNumber int64     `json:"parent_one_house_number"`
-	ParentOnePostcode    string    `json:"parent_one_postcode"`
-	ParentOneLocation    string    `json:"parent_one_location"`
-	ParentTwoName        string    `json:"parent_two_name"`
-	ParentTwoEmail       string    `json:"parent_two_email"`
-	ParentTwoPhone       int64     `json:"parent_two_phone"`
-	ParentTwoStreet      string    `json:"parent_two_street"`
-	ParentTwoHouseNumber int64     `json:"parent_two_house_number"`
-	ParentTwoPostcode    string    `json:"parent_two_postcode"`
-	ParentTwoLocation    string    `json:"parent_two_location"`
-}
-
 
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,11 +16,17 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	registerData := RegisterInput {
+	// create an instance of the struct
+	registerData := models.RegisterInput {
 		Name: r.FormValue("name"),
 		Birthday: r.FormValue("birthday"),
 		Origin: r.FormValue("origin"),
 	}
+
+	// print the data to the console
+	log.Println("Received form submission:", registerData)
+
+	fmt.Fprintf(w, "Form submitted successfully!")
 }
 
 func main() {
