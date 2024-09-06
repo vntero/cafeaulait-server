ed471dcc61983ed627fb1df29a95344eb0299eb2
vntero <shin.antero@gmail.com>
2024-09-06 11:38:14 +0200

    refactors registerHandler to accept json from client

--- functions/registerHandler.go
+++ functions/registerHandler.go
@@ -2,42 +2,33 @@ package functions
 
 import (
 	"cafeaulait-server/models"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	// parse incoming data
-	if err := r.ParseForm(); err != nil {
-		log.Println("Error parsing data:", err)
-		http.Error(w, "Unable to process data", http.StatusBadRequest)
-		return
+	EnableCors(&w)
+
+	if r.Method == http.MethodPost {
+
+		// decode incoming JSON
+		var registerData models.RegisterInput
+		err := json.NewDecoder(r.Body).Decode(&registerData)
+
+		if err != nil {
+			log.Println("Error decoding JSON:", err)
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+
+		// log decoded data for debugging
+		log.Println("Received Register form submission:", registerData)
+
+		// confirm to client that data has been received
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(registerData)
+
+		SendRegisterEmail(registerData)
 	}
-
-	// create an instance of the struct
-	registerData := models.RegisterInput{
-		Name:                 r.FormValue("name"),
-		Birthday:             r.FormValue("birthday"),
-		Origin:               r.FormValue("origin"),
-		Motivation:           r.FormValue("motivation"),
-		ParentOneName:        r.FormValue("parent_one_name"),
-		ParentOneEmail:       r.FormValue("parent_one_email"),
-		ParentOnePhone:       r.FormValue("parent_one_phone"),
-		ParentOneStreet:      r.FormValue("parent_one_street"),
-		ParentOneHouseNumber: r.FormValue("parent_one_house_number"),
-		ParentOnePostcode:    r.FormValue("parent_one_postcode"),
-		ParentOneLocation:    r.FormValue("parent_one_location"),
-		ParentTwoName:        r.FormValue("parent_two_name"),
-		ParentTwoEmail:       r.FormValue("parent_two_email"),
-		ParentTwoPhone:       r.FormValue("parent_two_phone"),
-		ParentTwoStreet:      r.FormValue("parent_two_street"),
-		ParentTwoHouseNumber: r.FormValue("parent_two_house_number"),
-		ParentTwoPostcode:    r.FormValue("parent_two_postcode"),
-		ParentTwoLocation:    r.FormValue("parent_two_location"),
-	}
-
-	// print the data to the console
-	log.Println("Received Register form submission:", registerData)
-
-	SendRegisterEmail(registerData)
 }
