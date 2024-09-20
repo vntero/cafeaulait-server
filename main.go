612f802b5a0810be6fcda454164d63c9c6144f18
vntero <shin.antero@gmail.com>
2024-09-20 22:28:57 +0200

    adds 1 to server port

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	routes.PostBook()
 	routes.PostRegister()
 
-	log.Println("Starting server on :8080")
+	log.Println("Starting server on :80801")
 
 	http.ListenAndServe(":8080", nil) // start the server on port 8080
 }
