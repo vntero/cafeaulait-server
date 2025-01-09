a03a7fa756014389a03923dbfe8b79765371257e
vntero <shin.antero@gmail.com>
2025-01-09 13:08:52 +0100

    adds required fields to register form

--- data/registerData.go
+++ data/registerData.go
@@ -1,17 +1,17 @@
 package data
 
 type ResgisterData struct {
-	Name                 string `json:"name"`
-	Birthday             string `json:"birthday"`
+	Name                 string `json:"name" validate:"required"`
+	Birthday             string `json:"birthday" validate:"required"`
 	Origin               string `json:"origin"`
 	Motivation           string `json:"motivation"`
-	ParentOneName        string `json:"parent_one_name"`
-	ParentOneEmail       string `json:"parent_one_email"`
-	ParentOnePhone       string `json:"parent_one_phone"`
-	ParentOneStreet      string `json:"parent_one_street"`
-	ParentOneHouseNumber string `json:"parent_one_house_number"`
-	ParentOnePostcode    string `json:"parent_one_postcode"`
-	ParentOneLocation    string `json:"parent_one_location"`
+	ParentOneName        string `json:"parent_one_name" validate:"required"`
+	ParentOneEmail       string `json:"parent_one_email" validate:"required"`
+	ParentOnePhone       string `json:"parent_one_phone" validate:"required"`
+	ParentOneStreet      string `json:"parent_one_street" validate:"required"`
+	ParentOneHouseNumber string `json:"parent_one_house_number" validate:"required"`
+	ParentOnePostcode    string `json:"parent_one_postcode" validate:"required"`
+	ParentOneLocation    string `json:"parent_one_location" validate:"required"`
 	ParentTwoName        string `json:"parent_two_name"`
 	ParentTwoEmail       string `json:"parent_two_email"`
 	ParentTwoPhone       string `json:"parent_two_phone"`
