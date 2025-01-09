b3ee3b17e1764ab88729d550b326d8690cdfbcb7
vntero <shin.antero@gmail.com>
2025-01-09 12:29:56 +0100

    improves book handler

--- handlers/addBook.go
+++ handlers/addBook.go
@@ -2,29 +2,42 @@ package handlers
 
 import (
 	"cafeaulait-server/configs"
-	"cafeaulait-server/models"
+	"cafeaulait-server/data"
 
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/go-playground/validator"
 )
 
 func AddBook(w http.ResponseWriter, r *http.Request) {
 	configs.EnableCors(&w)
 
-	if r.Method == http.MethodPost {
+	// responds to preflight OPTIONS requests
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 
+	// check if it's a POST method
+	if r.Method == http.MethodPost {
 		// Decode the incoming JSON payload into the struct directly
-		var bookData models.BookInput
+		var bookData data.BookData
 		err := json.NewDecoder(r.Body).Decode(&bookData)
 		if err != nil {
 			log.Println("Error decoding JSON:", err)
-			http.Error(w, "Bad request", http.StatusBadRequest)
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
 			return
 		}
 
-		// Log the received data for debugging
-		log.Println("Received Book form submission:", bookData)
+		// Validate incoming data
+		validate := validator.New()
+		if err := validate.Struct(bookData); err != nil {
+			log.Printf("Invalid data: %v", bookData)
+			http.Error(w, "Invalid request data", http.StatusBadRequest)
+			return
+		}
 
 		// Respond with the received data as confirmation
 		w.WriteHeader(http.StatusOK)
@@ -32,5 +45,7 @@ func AddBook(w http.ResponseWriter, r *http.Request) {
 
 		// Send an email with the received data
 		SendBookEmail(bookData)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
