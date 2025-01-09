df899c5f14b7bdf66ad3e2a58e94e81063b37440
vntero <shin.antero@gmail.com>
2025-01-09 12:41:35 +0100

    improves register handler

--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module cafeaulait-server
 
 go 1.21.1
 
-require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
 
 require (
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/joho/godotenv v1.5.1 // indirect
 	gopkg.in/mail.v2 v2.3.1
 )
--- handlers/addRegister.go
+++ handlers/addRegister.go
@@ -2,34 +2,49 @@ package handlers
 
 import (
 	"cafeaulait-server/configs"
-	"cafeaulait-server/models"
+	"cafeaulait-server/data"
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/go-playground/validator"
 )
 
 func AddRegister(w http.ResponseWriter, r *http.Request) {
 	configs.EnableCors(&w)
 
+	// Handle preflight OPTIONS request
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// check if it's a POST method
 	if r.Method == http.MethodPost {
-
-		// decode incoming JSON
-		var registerData models.RegisterInput
+		// Decode the incoming JSON payload into the struct directly
+		var registerData data.ResgisterData
 		err := json.NewDecoder(r.Body).Decode(&registerData)
-
 		if err != nil {
 			log.Println("Error decoding JSON:", err)
-			http.Error(w, "Bad request", http.StatusBadRequest)
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
 			return
 		}
 
-		// log decoded data for debugging
-		log.Println("Received Register form submission:", registerData)
+		// Validate incoming data
+		validate := validator.New()
+		if err := validate.Struct(registerData); err != nil {
+			log.Printf("Invalid data: %v", registerData)
+			http.Error(w, "Invalid request data", http.StatusBadRequest)
+			return
+		}
 
 		// confirm to client that data has been received
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(registerData)
 
+		// Send an email with the received data
 		SendRegisterEmail(registerData)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
