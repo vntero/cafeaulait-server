8f247facbbd2a91564956a17588fd3e249ad9aed
vntero <shin.antero@gmail.com>
2025-01-09 11:13:47 +0100

    improves cors config file

--- configs/cors.go
+++ configs/cors.go
@@ -3,6 +3,8 @@ package configs
 import "net/http"
 
 func EnableCors(w *http.ResponseWriter) {
+	// for local development -> http://localhost:5173
+	// for prod -> https://stehliantero.com
 	(*w).Header().Set("Access-Control-Allow-Origin", "*") // Allows requests from any origin
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
