1d8dec999446276c5b8f0ae2980e6d6dd0b381e5
vntero <shin.antero@gmail.com>
2025-01-17 17:07:26 +0100

    adds signature to both emails being generated

--- handlers/bookMailer.go
+++ handlers/bookMailer.go
@@ -12,17 +12,17 @@ import (
 )
 
 func SendBookEmail(data data.BookData) {
-	// load the env file
 	configs.LoadEnv()
 
-	// sender data
-	sender := os.Getenv("SENDER")
-	password := os.Getenv("PASSWORD")
-	receiver := os.Getenv("RECEIVER")
-	server := os.Getenv("SERVER")
+	sender := os.Getenv("SENDER_ICLOUD")
+	password := os.Getenv("PASSWORD_ICLOUD")
+	server := os.Getenv("SERVER_ICLOUD")
 
-	// email body
-	body := fmt.Sprintf(`
+	receiverCal := os.Getenv("RECEIVER_ICLOUD")
+	receiverCustomer := data.Email
+
+	// Internal email body
+	businessBody := fmt.Sprintf(`
 		<html>
 		<head>
 			<style>
@@ -97,30 +97,116 @@ func SendBookEmail(data data.BookData) {
 		data.Name, data.Phone, data.Email, data.Organization, data.Location, data.Duration,
 		data.NumberOfGuests, data.EventDate, data.EventTime, data.Budget, data.Comment)
 
-	// gomail magic starts here
-	m := gomail.NewMessage()
+	// External email body
+	customerBody := fmt.Sprintf(`
+		<html>
+		<head>
+			<style>
+			body {
+				font-family: Arial, sans-serif;
+				line-height: 1.6;
+				color: #333333;
+			}
+			.container {
+				max-width: 600px;
+				margin: auto;
+				border: 1px solid #dddddd;
+				padding: 20px;
+				border-radius: 10px;
+				background-color: white;
+			}
+			.header {
+				text-align: center;
+				color: #EF4444; /* Matches Tailwind text-red-500 */
+				padding: 10px 0;
+				font-weight: 500; /* Matches Tailwind font-medium */
+			}
+			.section {
+				margin-top: 20px;
+			}
+			h1, h3 {
+				color: #EF4444; /* Matches Tailwind text-red-500 */
+				font-weight: 500; /* Matches Tailwind font-medium */
+			}
+			ul {
+				list-style: none;
+				padding: 0;
+			}
+			ul li {
+				margin-bottom: 8px;
+			}
+			.footer {
+				text-align: center;
+				margin-top: 20px;
+				font-size: 12px;
+				color: #aaaaaa;
+			}
+			</style>
+		</head>
+		<body>
+			<div class="container">
+			<div class="header">
+				<h1>Vielen Dank für Ihre Buchungsanfrage</h1>
+			</div>
+			<div class="section">
+				<h3>Wir haben folgende Details erhalten:</h3>
+				<ul>
+				<li><strong>Name:</strong> %s</li>
+				<li><strong>Telefonnummer:</strong> %s</li>
+				<li><strong>Emailadresse:</strong> %s</li>
+				<li><strong>Organisation:</strong> %s</li>
+				<li><strong>Veranstaltungsort:</strong> %s</li>
+				<li><strong>Gewünschte Dauer der Darbietung:</strong> %s</li>
+				<li><strong>Gästeanzahl:</strong> %s</li>
+				<li><strong>Veranstaltungsdatum:</strong> %s</li>
+				<li><strong>Veranstaltungszeit:</strong> %s</li>
+				<li><strong>Budget:</strong> %s</li>
+				<li><strong>Zusätzliche Infos oder Fragen:</strong> %s</li>
+				</ul>
+			</div>
+			<div class="signature">
+		  		<br></br>
+				<p>Wir werden uns in Kürze bei Ihnen melden.</p>
+				<p>Liebe Grüsse,</p>
+        		<p>Café Au Lait Team</p>
+		  </div>
+			<div class="footer">
+				<p>Dies ist eine automatisch generierte. Bestätigung Ihrer Anfrage.</p>
+			</div>
+			</div>
+		</body>
+		</html>`,
+		data.Name, data.Phone, data.Email, data.Organization, data.Location, data.Duration,
+		data.NumberOfGuests, data.EventDate, data.EventTime, data.Budget, data.Comment)
 
-	// Set E-Mail sender
-	m.SetHeader("From", sender)
+	// Send to business
+	m1 := gomail.NewMessage()
+	m1.SetHeader("From", sender)
+	m1.SetHeader("To", receiverCal)
+	m1.SetHeader("Subject", "Neue Buchungsanfrage eingegangen")
+	m1.SetBody("text/html", businessBody)
 
-	// Set E-Mail receivers
-	m.SetHeader("To", receiver)
-
-	// Set E-Mail subject
-	m.SetHeader("Subject", "Neue Buchungsanfrage eingegangen")
-
-	// Set E-Mail body. You can set plain text or html with text/html
-	m.SetBody("text/html", body)
+	// Send to customer
+	m2 := gomail.NewMessage()
+	m2.SetHeader("From", sender)
+	m2.SetHeader("To", receiverCustomer)
+	m2.SetHeader("Subject", "Bestätigung Ihrer Buchungsanfrage")
+	m2.SetBody("text/html", customerBody)
 
 	// Settings for SMTP server
-	d := gomail.NewDialer(server, 587, sender, password)
-
 	// This is only needed when SSL/TLS certificate is not valid on server.
 	// In production this should be set to false.
+	d := gomail.NewDialer(server, 587, sender, password)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-	// Now send E-Mail
-	if err := d.DialAndSend(m); err != nil {
+	// Send email to business
+	if err := d.DialAndSend(m1); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	// Send email to customer
+	if err := d.DialAndSend(m2); err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
--- handlers/registerMailer.go
+++ handlers/registerMailer.go
@@ -14,14 +14,15 @@ import (
 func SendRegisterEmail(data data.ResgisterData) {
 	configs.LoadEnv()
 
-	sender := os.Getenv("SENDER")
-	password := os.Getenv("PASSWORD")
-	server := os.Getenv("SERVER")
+	sender := os.Getenv("SENDER_ICLOUD")
+	password := os.Getenv("PASSWORD_ICLOUD")
+	server := os.Getenv("SERVER_ICLOUD")
 
-	receiver := os.Getenv("RECEIVER")
+	receiverCal := os.Getenv("RECEIVER_ICLOUD")
+	receiverCustomer := data.ParentOneEmail
 
-	// Email body
-	body := fmt.Sprintf(`
+	// Internal email body
+	businessBody := fmt.Sprintf(`
 	<html>
 	  <head>
 		<style>
@@ -116,30 +117,136 @@ func SendRegisterEmail(data data.ResgisterData) {
 		data.ParentTwoName, data.ParentTwoEmail, data.ParentTwoPhone, data.ParentTwoStreet,
 		data.ParentTwoHouseNumber, data.ParentTwoPostcode, data.ParentTwoLocation)
 
-	// gomail magic starts here
-	m := gomail.NewMessage()
+	// External email body
+	customerBody := fmt.Sprintf(`
+	<html>
+		<head>
+			<style>
+			body {
+				font-family: Arial, sans-serif;
+				line-height: 1.6;
+				color: #333333;
+			}
+			.container {
+				max-width: 600px;
+				margin: auto;
+				border: 1px solid #dddddd;
+				padding: 20px;
+				border-radius: 10px;
+				background-color: white;
+			}
+			.header {
+				text-align: center;
+				color: #EF4444;
+				padding: 10px 0;
+				font-weight: 500;
+			}
+			.section {
+				margin-top: 20px;
+			}
+			h1, h2 {
+				color: #EF4444;
+				font-weight: 500;
+			}
+			ul {
+				list-style: none;
+				padding: 0;
+			}
+			ul li {
+				margin-bottom: 8px;
+			}
+			.footer {
+				text-align: center;
+				margin-top: 20px;
+				font-size: 12px;
+				color: #aaaaaa;
+			}
+			</style>
+		</head>
+		<body>
+			<div class="container">
+			<div class="header">
+				<h1>Vielen Dank für Ihr Interesse</h1>
+			</div>
+			<div class="section">
+				<h2>Wir haben folgende Details erhalten:</h2>
+				<ul>
+				<li><strong>Name:</strong> %s</li>
+				<li><strong>Geburtsdatum:</strong> %s</li>
+				<li><strong>Herkunft:</strong> %s</li>
+				<li><strong>Motivationsgrund:</strong> %s</li>
+				</ul>
+			</div>
+			<div class="section">
+				<strong>Erziehungsberechtigte:r 1</strong>
+				<ul>
+				<li><strong>Name:</strong> %s</li>
+				<li><strong>Email:</strong> %s</li>
+				<li><strong>Telefonnummer:</strong> %s</li>
+				<li><strong>Strasse:</strong> %s</li>
+				<li><strong>Nr:</strong> %s</li>
+				<li><strong>PLZ:</strong> %s</li>
+				<li><strong>Ort:</strong> %s</li>
+				</ul>
+			</div>
+			<div class="section">
+				<strong>Erziehungsberechtigte:r 2</strong>
+				<ul>
+				<li><strong>Name:</strong> %s</li>
+				<li><strong>Email:</strong> %s</li>
+				<li><strong>Telefonnummer:</strong> %s</li>
+				<li><strong>Strasse:</strong> %s</li>
+				<li><strong>Nr:</strong> %s</li>
+				<li><strong>PLZ:</strong> %s</li>
+				<li><strong>Ort:</strong> %s</li>
+				</ul>
+			</div>
+			<div class="signature">
+					<br></br>
+					<p>Wir werden uns in Kürze bei Ihnen melden.</p>
+					<p>Liebe Grüsse,</p>
+					<p>Café Au Lait Team</p>
+			</div>
+			<div class="footer">
+				<p>Diese Nachricht wurde automatisch generiert. Bitte nicht darauf antworten.</p>
+			</div>
+			</div>
+		</body>
+	</html>`,
+	data.Name, data.Birthday, data.Origin, data.Motivation,
+	data.ParentOneName, data.ParentOneEmail, data.ParentOnePhone, data.ParentOneStreet,
+	data.ParentOneHouseNumber, data.ParentOnePostcode, data.ParentOneLocation,
+	data.ParentTwoName, data.ParentTwoEmail, data.ParentTwoPhone, data.ParentTwoStreet,
+	data.ParentTwoHouseNumber, data.ParentTwoPostcode, data.ParentTwoLocation)
 
-	// Set E-Mail sender
-	m.SetHeader("From", sender)
+	// Send to business
+	m1 := gomail.NewMessage()
+	m1.SetHeader("From", sender)
+	m1.SetHeader("To", receiverCal)
+	m1.SetHeader("Subject", "Ein weiteres Kind möchte bei uns mittanzen")
+	m1.SetBody("text/html", businessBody)
 
-	// Set E-Mail receiver
-	m.SetHeader("To", receiver)
-
-	// Set E-Mail subject
-	m.SetHeader("Subject", "Ein weiteres Kind möchte bei uns mittanzen")
-
-	// Set E-Mail body. You can set plain text or html with text/html
-	m.SetBody("text/html", body)
+	// Send to customer
+	m2 := gomail.NewMessage()
+	m2.SetHeader("From", sender)
+	m2.SetHeader("To", receiverCustomer)
+	m2.SetHeader("Subject", "Vielen Dank für Ihr Interesse")
+	m2.SetBody("text/html", customerBody)
 
 	// Settings for SMTP server
-	d := gomail.NewDialer(server, 587, sender, password)
-
 	// This is only needed when SSL/TLS certificate is not valid on server.
 	// In production this should be set to false.
+	d := gomail.NewDialer(server, 587, sender, password)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-	// Now send E-Mail
-	if err := d.DialAndSend(m); err != nil {
+	// Send email to business
+	if err := d.DialAndSend(m1); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	// Send email to customer
+	if err := d.DialAndSend(m2); err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
